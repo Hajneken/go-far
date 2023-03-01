ea616a6796677479b9cfc9377165abe924b609f5
Hynek Zemanec <25231320+Hajneken@users.noreply.github.com>
2023-03-01 17:35:56 +0100

    more of syntax cheat sheet, some Readme gotchas

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Excercises in Go
 
-This repository contains a collection of excercises in Go.
+This repository contains a collection of exercises and reference material for [Go](https://go.dev/).
+
+See [Syntax Cheatsheet](syntaxCheatsheet.go) for all Go syntax on 1 page.
 
 Excercises are seperated by directories named using the following naming convention `<number>_<ExcerciseName>`
 
@@ -9,4 +11,33 @@ Excercises are seperated by directories named using the following naming convent
 
 ## Biased opinion
 
-> Go is simply amazing! Think Python simplicity with C++ performance and zero Java BS and no JavaScript esotericism. Testing, documentation and package management ships with the language. Also compiles to binary. What more can you possibly want? Composition over Inheritance every time baby! I love it! Let's Go!
+> Go is simply amazing! Think Python simplicity with C++ performance, Rust memory safety, no Java BS and no JavaScript esotericism. Testing, documentation and package management ships with the language. Also compiles to binary. What more can you possibly want? Composition over Inheritance every time baby! I love it! Let's Go!
+
+## Quick Go types Cheatsheet 
+
+<table><thead><tr><th>Type</th><th>Description</th></tr></thead><tbody><tr><td><code>bool</code></td><td>Boolean, either <code>true</code> or <code>false</code></td></tr><tr><td><code>int</code></td><td>Signed integer, either 32 or 64 bits</td></tr><tr><td><code>uint</code></td><td>Unsigned integer, either 32 or 64 bits</td></tr><tr><td><code>float32</code></td><td>IEEE-754 32-bit floating-point number</td></tr><tr><td><code>float64</code></td><td>IEEE-754 64-bit floating-point number</td></tr><tr><td><code>complex64</code></td><td>Complex number with float32 real and imaginary parts</td></tr><tr><td><code>complex128</code></td><td>Complex number with float64 real and imaginary parts</td></tr><tr><td><code>byte</code></td><td>Alias for <code>uint8</code>, represents a byte</td></tr><tr><td><code>rune</code></td><td>Alias for <code>int32</code>, represents a Unicode code point</td></tr><tr><td><code>string</code></td><td>A string of characters</td></tr><tr><td><code>error</code></td><td>An interface type that represents an error condition</td></tr><tr><td><code>uintptr</code></td><td>An unsigned integer that is large enough to hold the bit pattern of any pointer</td></tr></tbody></table>
+
+## OO in Go Gotchas
+
+- there are no classes in Go
+- there is only `struct` type that have the same function
+- there is no concept of inheritance in Go
+- structs can be composed (composition over inheritance)
+
+## Interfaces Gotchas
+
+> instead of designing our abstractions in terms of what kind of data our types can hold, we design our abstractions in terms of what actions our types can execute. -[Jordan Orel](https://jordanorelli.com/post/32665860244/how-to-use-interfaces-in-go)
+
+- no `implements` keyword 
+- if given `struct` has all methods from given interface defined it implements it 
+- every type satisfy at least an empty interface `interface{}`
+
+you can pass anything to the function below, but v is not `any` type but `interface{}` type
+```Go
+func myFunc(v interface{}){
+    ...
+}
+
+```
+
+everything in Go is passed by value
--- syntaxCheatsheet.go
+++ syntaxCheatsheet.go
@@ -24,11 +24,97 @@ var print = fmt.Println
 // variables outside of functions are global variables
 var globalGreeting string = "Hello World"
 
+/* STRUCTS */
+
+// structs are used instead of classes, they have the same purpose
+// but inheretance is not supported
+type name struct {
+	firstName  string
+	middleName string
+	lastName   string
+}
+
+// go embraces composition over inheritance, i.e. put types together instead of inheriting from them
+// capital letters are used to denote public fields, i.e. Person is exported and can be used elsewhere
+type Person struct {
+	// structs can be embedded in other structs
+	name name
+	age  int
+}
+
+// idiomatic code == code that is natural and typical for a language, most effective to express a solution
+// it is idomatic to use constructor function that by convention starts with `New`
+// notice that the constructor function is declared outside of main lexical scope
+func NewPerson(name name, age int) *Person {
+	person := Person{}
+	person.name = name
+	person.age = age
+	return &person
+}
+
+// methods have a somewhat esotheric syntax `func (Type type) name()`
+func (p *Person) introduce() string {
+	return "My name is " + p.name.firstName + " " + p.name.lastName
+}
+
+// notice there is no `this` keyword in Go, lowercase letter is used instead
+func (p *Person) squareAge() int {
+	return int(math.Pow(float64(p.age), 2))
+}
+
+// printItAll outputs sequentially all passed strings into console
+// go supports variadic functions: function that can have indefinite number of arguments
+func printItAll(words ...string) {
+	// note that `_` is a blank identifier, by convention it signifies "throwaway" variable that I don't need or use specifically here the index
+	for _, w := range words {
+		fmt.Println(w)
+	}
+}
+
+// anonymous functions are supported
+func countParams(params ...string) func() int {
+	// had we declared a variable here that would be used by the returned anonymous function
+	// we would have created a closure -> access to variables after returning
+	return func() int {
+		return len(params)
+	}
+
+}
+
+/* INTERFACES */
+
+// interface is a wrapper for method signatures, if struct has all methods defined in interface it is considered implemented
+
+// Animal has a method speak
+type Animal interface {
+	Speak() string
+}
+
+type Dog struct {
+}
+
+type Cat struct {
+}
+
+// if method Speak is defined it is automatically considered implemented from Animal type
+func (d Dog) Speak() string {
+	return "Woof"
+}
+
+func (c Cat) Speak() string {
+	return "Meow"
+}
+
+/* GENERICS */
+
+// instead of defining several functions each with it's own type let's create a generic function
+// T is can be either int or float64 -> corresponding code is `generated`
+func add[T int | float64](a T, b T) T {
+	return a + b
+}
+
 // main function is the entry point for the program
 func main() {
-	// print to console, if you like typing use fmt.Println, otherwise use print alias we declared above
-	fmt.Println("Hello World")
-
 	/* VARIABLES */
 
 	// declare variable like this
@@ -53,40 +139,16 @@ func main() {
 	// with const you can do arithmetic with arbitrary precision
 	print("Pi is", pi, "and 2*pi is", 2*pi)
 
-	/* DATA TYPES */
+	// using functions
 
-	// structs are used instead of classes, they have the same purpose
-	// but inheretance is not supported
-	type name struct {
-		firstName  string
-		middleName string
-		lastName   string
-	}
+	// function that can take indefinite amount of arguments
+	printItAll("So", "Much", "to", "say", "so", "little", "time")
 
-	// go embraces composition over inheritance, i.e. put types together instead of inheriting from them
-	// capital letters are used to denote public fields, i.e. Person is exported and can be used elsewhere
-	type Person struct {
-		// structs can be embedded in other structs
-		name name
-		age  int
-	}
+	var parametersNumber int = countParams("some", "things", "never", "change")()
+	// print to console, if you like typing use fmt.Println, otherwise use print alias we declared above
+	fmt.Println("the number of passed parameters", parametersNumber)
 
-	// initialize object of type person
-	john := Person{name{"John", "Doe", "Smith"}, 42}
-	// the default toString() method is sensible
-	print("We consider the following person:", john)
-	// we can write out non-string values like this
-	print("The age of "+john.name.firstName+" is", john.age, "years old")
-	// or format the string before like this
-	hisLooksDecieves := fmt.Sprintf("But really he looks like %v", john.age-10)
-	// and print it out
-	print(hisLooksDecieves)
-
-	// let's properly log informative message
-	log.Println("Yo, computer here, this app looks solid to me! Btw. John looks more like", john.age-20, "to me")
-
-	// some quick maths
-	print("The square root of 4 is", math.Sqrt(4))
+	/* ARRAYS */
 
 	// arrays are fixed length
 	array := [5]int{1, 2, 3, 4, 5}
@@ -113,6 +175,11 @@ func main() {
 
 	// great thing is that both slices and arrays are intially zeroed out
 	print("Empty slice folks", slice2) // prints [0 0 0 0 0]
+
+	// change second element to 1
+	slice2[1] = 1
+
+	print("Changed the second index of an array", slice2)
 
 	// multidimensional arrays are supported
 	squareMatrixRank3 := [3][3]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
@@ -170,7 +237,7 @@ func main() {
 		print("This is the same as above")
 	}
 
-	// sadly no terinary operator
+	// sadly no tertiary operator
 
 	/* SWITCH STATEMENTS */
 
@@ -215,5 +282,72 @@ func main() {
 
 	figureOutType(1)
 	figureOutType("Hello World")
+
+	myArray := make([]int, 5)
+
+	print("The length of the array", len(myArray))
+
+	/* POINTERS */
+
+	myInt := 1
+	// pointers work the same as in C++
+	var ptrIntVar *int = &myInt
+
+	print("This is a pointer of an int: ", &myInt)
+	print("This is the same memory address: ", ptrIntVar)
+	print("Print the value a.k.a. indirection of ptrIntVar using *ptrIntVar:", *ptrIntVar)
+
+	/* STRUCTS usage */
+
+	hynek := NewPerson(name{"Hynek", "", "Zemanec"}, 25)
+
+	print("HYNEK", hynek)
+
+	print(hynek.introduce())
+
+	print("Imagine living until square root of your age: ", hynek.squareAge())
+
+	// initialize object of type person
+	john := Person{name{"John", "Doe", "Smith"}, 42}
+	// the default toString() method is sensible
+	print("We consider the following person:", john)
+	// we can write out non-string values like this
+	print("The age of "+john.name.firstName+" is", john.age, "years old")
+	// or format the string before like this
+	hisLooksDecieves := fmt.Sprintf("But really he looks like %v", john.age-10)
+	// and print it out
+	print(hisLooksDecieves)
+
+	// let's properly log informative message
+	log.Println("Yo, computer here, this app looks solid to me! Btw. John looks more like", john.age-20, "to me")
+
+	// some quick maths
+	print("The square root of 4 is", math.Sqrt(4))
+
+	print("This is a pointer to a struct: ", &john)
+
+	/* INTERFACES usage */
+
+	dog := Dog{}
+
+	print("The dog goes", dog.Speak())
+	print("The cat goes ", dog.Speak())
+
+	// using interface we can put similar things together in collection
+	animals := []Animal{Dog{}, Cat{}}
+
+	for _, animal := range animals {
+		print("Animal that does", animal.Speak())
+	}
+
+	/* GENERICS usage */
+
+	genericIntResult := add(2, 3)
+	genericFloatResult := add(2.2, 3.4)
+	betterGeneric := addImproved(2, 3.2)
+
+	print(genericIntResult)
+	print(genericFloatResult)
+	print(betterGeneric)
 
 }
